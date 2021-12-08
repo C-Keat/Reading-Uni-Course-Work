31310867c04c4d2dc77873c81b2bba0a5c0d68d5
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-12-08 15:08:55 +0000

    Questions 1 finished

--- Maths and Statisitic Assignment/Assignment Solutions.R
+++ Maths and Statisitic Assignment/Assignment Solutions.R
@@ -1,8 +1,19 @@
 ##########################################################
 #Question 1
+#Defining the Matrix
 A<-matrix(c(3,-1,0,-1,0,-1,3,-1,3,-1,0,-1,3,1,0,-1,3,1,5,-1,0,-1,0,-1,1), nrow=5)
 
-
+#Function that can be passed Matx in order to perform Cholesky Functions
+choleskyFunction <- function(Matx){
+  #Finds the upper triangular matrix
+  L<-chol(Matx)
+  #Transposes the matrix to show the lower triangular matrix
+  Lt<-t(L)
+  #Displays the lower hand matrix
+  Lt
+}
+#Runs the function
+choleskyFunction(A)
 
 
 ##########################################################
