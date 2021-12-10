7f2460b332601cf87f0733de50249de7dc6cb1da
C-Keat <86522563+C-Keat@users.noreply.github.com>
2021-12-10 16:19:57 +0000

    Question 5 added (incomplete)

--- Maths and Statisitic Assignment/Assignment Solutions.R
+++ Maths and Statisitic Assignment/Assignment Solutions.R
@@ -1,4 +1,4 @@
-##########################################################
+####################Question 1############################
 #Question 1
 #Defining the Matrix
 A<-matrix(c(3,-1,0,-1,0,-1,3,-1,3,-1,0,-1,3,1,0,-1,3,1,5,-1,0,-1,0,-1,1), nrow=5)
@@ -14,7 +14,7 @@ choleskyFunction <- function(Matx){
 }
 #Runs the function
 choleskyFunction(A)
-##########################################################
+####################Question 2############################
 #Question 2 
 #Singular value Decompostion
 #the creation of matrix
@@ -25,4 +25,46 @@ Q2Matrix
 sv <-svd(Q2Matrix)
 #Displaying Singular values
 sv
+####################Question 3############################
+#Question 3
+####################Question 4############################
+#Question 4
+####################Question 5#############################
+#Question 5
+#Function plot - Plot the formula below
+Question5F<-function(x,y,a,b) (x^2+y-11)^2+(x+y^2-7)^2
+#defining the x,y and z of the measurements
+x<-seq(-4.5,4.5,length.out = 100)
+y<-seq(-4.5,4.5,length.out = 100)
+z<-outer(x,y,Question5F,a=1,b=100)
+
+#plotting the graph 
+persp(x,y,z,theta =-45,phi=45,expand=0.5,col="yellow",shade =0.65,ticktype ="detailed",xlab ="x",ylab ="y",zlab="z")
+
+#Writing the function so x,y is a vector
+Question5FV<-function(vec) (vec[1]^2+vec[2]-11)^2+(vec[1]+vec[2]^2-7)^2
+
+#Minimum can be found based on value being 0 or so close to 0
+
+#Based on plot there seem to be 4 dips, each minimum is extracted here
+
+#From plot minimum seems to be near x=-4,y=-4.
+minimum1<- optim(c(-4,-4),Question5FV)$par
+minimum1
+
+#From plot minimum seems to be near x = 4, y= -3.5
+minimum2 <- optim(c(4,-3.5),Question5FV)$par
+minimum2
+
+#From plot minimum seems to be near x = -3.5, y = 3.5
+minimum3 <- optim(c(-3.5, 3.5),Question5FV)$par
+minimum3
+
+#From plot minimum seems to be near x = 3.5, y = 3.5
+minimum4 <- optim(c(3.5,3.5),Question5FV)$par
+minimum4
+
+#creating a list of minimums
+minimum_list <- list(minimum1,minimum2,minimum3,minimum4)
+minimum_list
 ##########################################################
